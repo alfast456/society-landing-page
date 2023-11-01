ae0cb2783ab8d48be57aa3a89f51491fb971952f
Alfa hardiyansyah <56823267+alfast456@users.noreply.github.com>
2023-11-01 20:33:18 +0700

    Update countdown-custom.js

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2023, 10, 5, 0)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2023, 10, 6, 0)}); // year, month, date, hour
         });
 });		
 
